ec56a41fde8672cfb03ec21128796910b80a2fb1
greg <gradek64@yahoo.com>
2017-07-23 18:49:46 +0100

    final touches

--- views/js/angular/page_scripts/createUser/ang_ngMessages_password_visibility.js
+++
@@ -1,8 +0,0 @@
-
-      angular.module('app', ['ngMessages'])
-      .controller('MainCtrl', function($scope){
-
-        $scope.passwordType = "password";
-        $scope.togglePasswordVisibility = function(){ $scope.passwordType = $scope.showEye ? "text" : "password"; } //show password or text depend on ShowEye*/
-
-    })//controller;
